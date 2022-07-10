955efb2eadfb9d0652e7d88dcd72521618f5b139
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 21:35:31 +0300

    Sukurta ir istestuota skaiciavimo funkcija (pagal salyga)

--- src/Uzduotis2.java
+++ src/Uzduotis2.java
@@ -5,8 +5,34 @@
  */
 public class Uzduotis2 {
     public static void main(String[] args) {
-
+        System.out.println("skaiciavimas(15, 95) = " + skaiciavimas(15, 95));
+        System.out.println("skaiciavimas(16, 95) = " + skaiciavimas(16, 95));
+        System.out.println("skaiciavimas(15, 105) = " + skaiciavimas(15, 105));
+        System.out.println("skaiciavimas(15, 100) = " + skaiciavimas(15, 100));
     }
+
+
+    /**
+     * Jeigu `x` yra lyginis skaičius, tai tada `funkcija` grąžina `x * y`. Jeigu `x` yra nelyginis skaičius, tai tada jeigu `y` yra didesnis už 100, funkcija grąžina `y - x`; jeigu `y < 100` tai grąžina `x - y`. Jeigu `y = 100`, tai grąžina `0`.
+     * @param x pirmas skaičius
+     * @param y antras skaičius
+     * @return rezultatą apskaičiuotą pagal sąlygą
+     */
+    public static int skaiciavimas(int x, int y) {
+        if (arLyginis(x)) {
+            return x * y;
+        }
+        else if (y > 100) {
+            return  y - x;
+        }
+        else if (y < 100) {
+            return x - y;
+        }
+        else {
+            return 0;
+        }
+    }
+
 
     /**
      * Funkcija patikrina ar skaičius yra lyginis.
