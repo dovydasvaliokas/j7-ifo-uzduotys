aa44cc35386d3bb2bcdcf948072af102f8cb29e7
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 21:30:45 +0300

    Main funkcija ir salyga

--- src/Uzduotis2.java
+++ src/Uzduotis2.java
@@ -1,3 +1,10 @@
-
+/*
+`Užduotis 2:`
+1. Susikurkite `Java` projektą, susikurkite `Programa.java` failą (class), tame faile susikurkite `main` funkciją.
+2. Susikurti papildomą funkciją, kuri gauna du skaičius `x` ir `y`. Jeigu `x` yra lyginis skaičius, tai tada `funkcija` grąžina `x * y`. Jeigu `x` yra nelyginis skaičius, tai tada jeigu `y` yra didesnis už 100, funkcija grąžina `y - x`; jeigu `y < 100` tai grąžina `x - y`. Jeigu `y = 100`, tai grąžina `0`.
+ */
 public class Uzduotis2 {
+    public static void main(String[] args) {
+
+    }
 }
